4204fd163d40078f0c17e52c0f54f37b8f290061
TsaoLun <ycl18@live.com>
2023-02-22 23:56:37 +0800

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,183 @@
+import { Head } from "$fresh/runtime.ts";
+import Counter from "../islands/Counter.tsx";
+import { JSX } from "preact";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import LemonIcon from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/lemon-2.tsx";
+import { ComponentChildren } from "preact";
+import BrandGithub from "https://deno.land/x/tabler_icons_tsx@0.0.1/tsx/brand-github.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-6">
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
+        </p>
+        <Counter start={3} />
+        <div class="flex gap-2 w-full">
+          <ColoredButton>点我</ColoredButton>
+          <LinkButton href="https://www.baidu.com">Go Baidu</LinkButton>
+          <Input />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export function ColoredButton(
+  props: JSX.HTMLAttributes<HTMLButtonElement>,
+) {
+  return (
+    <button
+      {...props}
+      disabled={!IS_BROWSER || props.disabled}
+      class="px-3 py-2 bg-blue-200 text-blue-800 rounded hover:bg-blue-300 active:bg-blue-400"
+    />
+  );
+}
+
+export function LinkButton(
+  props: JSX.HTMLAttributes<HTMLAnchorElement>,
+) {
+  return (
+    <a
+      {...props}
+      class={`inline-block cursor-pointer px-3 py-2 bg-white rounded hover:bg-gray-100 ${
+        props.class ?? ""
+      }`}
+    />
+  );
+}
+
+export function Input(props: JSX.HTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      {...props}
+      disabled={props.disabled}
+      class={`px-3 py-2 bg-white rounded border(gray-500 2) disabled:(opacity-50 cursor-not-allowed) ${
+        props.class ?? ""
+      }`}
+    />
+  );
+}
+
+type Props = {
+  active?: string;
+};
+
+export function Header({ active }: Props) {
+  const menus = [
+    { name: "Home", href: "/" },
+    { name: "Components", href: "/components" },
+    { name: "Docs", href: "/docs" },
+  ];
+
+  return (
+    <div class="bg-white w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
+      <div class="flex items-center flex-1">
+        <LemonIcon />
+        <div class="text-2xl  ml-1 font-bold">
+          Fresh
+        </div>
+      </div>
+      <ul class="flex items-center gap-6">
+        {menus.map((menu) => (
+          <li>
+            <a
+              href={menu.href}
+              class={"text-gray-500 hover:text-gray-700 py-1 border-gray-500" +
+                (menu.href === active ? " font-bold border-b-2" : "")}
+            >
+              {menu.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+type FooterProps = {
+  children?: ComponentChildren;
+};
+
+export function Footer({ children }: FooterProps) {
+  const menus = [
+    {
+      title: "Documentation",
+      children: [
+        { name: "Getting Started", href: "#" },
+        { name: "Guide", href: "#" },
+        { name: "API", href: "#" },
+        { name: "Showcase", href: "#" },
+        { name: "Pricing", href: "#" },
+      ],
+    },
+    {
+      title: "Community",
+      children: [
+        { name: "Forum", href: "#" },
+        { name: "Discord", href: "#" },
+      ],
+    },
+  ];
+
+  return (
+    <div class="bg-white flex flex-col md:flex-row w-full max-w-screen-lg gap-8 md:gap-16 px-8 py-8 text-sm">
+      <div class="flex-1">
+        <div class="flex items-center gap-1">
+          <LemonIcon class="inline-block" />
+          <div class="font-bold text-2xl">
+            Fresh
+          </div>
+        </div>
+        <div class="text-gray-500">
+          Full Stack Framework
+        </div>
+      </div>
+
+      {menus.map((item) => (
+        <div class="mb-4" key={item.title}>
+          <div class="font-bold">{item.title}</div>
+          <ul class="mt-2">
+            {item.children.map((child) => (
+              <li class="mt-2" key={child.name}>
+                <a
+                  class="text-gray-500 hover:text-gray-700"
+                  href={child.href}
+                >
+                  {child.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
+      <div class="text-gray-500 space-y-2">
+        <div class="text-xs">
+          Copyright © 2020 DenoLand<br />
+          All right reserved.
+        </div>
+
+        <a
+          href="https://github.com/denoland/fresh"
+          class="inline-block hover:text-black"
+        >
+          <BrandGithub />
+        </a>
+      </div>
+    </div>
+  );
+}
